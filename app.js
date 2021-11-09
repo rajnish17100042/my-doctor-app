2b696bac7a5c41d9effc4ff1b4cf1293c490108c
Rajnish Patel <rajnish17100042@gmail.com>
2021-11-09 15:20:42 +0530

    added home route for angular app

--- app.js
+++ app.js
@@ -36,10 +36,12 @@ app.use(cors());
 // });
 //for production
 if (process.env.NODE_ENV == "production") {
-  app.use(express.static("client/dist"));
+  app.use(express.static("client/dist/client"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+    res.sendFile(
+      path.resolve(__dirname, "client", "dist", "client", "index.html")
+    );
   });
 }
 app.listen(port, (err) => {
